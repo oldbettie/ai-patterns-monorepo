83ff2e6aaa82906f3b90cf477a66fac343108bc0
JJ Eaton <58495321+oldbettie@users.noreply.github.com>
2026-02-21 09:05:13 +0800

    remove unused page.

--- apps/web-app/app/sitemap.ts
+++ apps/web-app/app/sitemap.ts
@@ -11,12 +11,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1,
     },
     {
-      url: `${baseUrl}/why`,
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.7,
-    },
-    {
       url: `${baseUrl}/login`,
       lastModified: new Date(),
       changeFrequency: 'yearly',
